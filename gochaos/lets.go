0bb55c1fc780ff59051d5572d732587492f16628
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-14 22:55:27 +0200

    lets GO git 👾

--- gochaos/lets.go
+++ gochaos/lets.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"log"
+	"os"
 )
 
 var displayStuff = fmt.Println
@@ -9,4 +12,15 @@ var statement = "Lets GOOO!💨👽"
 
 func main() {
 	displayStuff(statement)
+	displayStuff("Whats your Name:")
+
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+
+	if err == nil {
+		displayStuff("Lets Go!", name)
+	} else {
+		log.Fatal(err)
+	}
+	
 }
