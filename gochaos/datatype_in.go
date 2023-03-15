4954880c601550709190de67e88aacf3eb176486
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-15 12:13:08 +0200

    learning datatypes in go 💨

--- gochaos/datatype_in.go
+++ gochaos/datatype_in.go
@@ -13,6 +13,12 @@ func main() {
 	// int, float64, bool, string, rune
 	// Default type 0, 0.0, false, ""
 	number := false
+	number1 := 1
+	number2 := 2.1
+	number3 := "sup"
 
 	displayStuff("The type of number is :", reflect.TypeOf(number))
+	displayStuff("The type of number1 is :", reflect.TypeOf(number1))
+	displayStuff("The type of number2 is :", reflect.TypeOf(number2))
+	displayStuff("The type of number3 is :", reflect.TypeOf(number3))
 }
