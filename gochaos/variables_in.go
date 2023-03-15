e20cd2e86e4b8d2a0ed9aef2ca2f379eed1c965f
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-15 11:44:14 +0200

    so go discourages an used variables... 👾👾

--- gochaos/variables_in.go
+++ gochaos/variables_in.go
@@ -10,5 +10,10 @@ func main() {
 	// comments in go
 
 	var variableName string = "Solomon"
+	var v1, v2 = 1.2, 5.6
+	var sum = v1 + v2
+
+
 	displayStuff("My name is :", variableName)
+	displayStuff("The sum is :", sum)
 }
