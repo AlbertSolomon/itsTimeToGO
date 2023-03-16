ab19ce40c782942d57dfaa7de579e8e1273a3e10
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-16 15:16:28 +0200

    lets save some battery 🔋

--- gochaos/conditions_in.go
+++ gochaos/conditions_in.go
@@ -11,4 +11,10 @@ func main() {
 
 	// Conditional Operators : > < >= <= == !=
 	// logical operators : && || !
+
+	age := 15
+
+	if age < 18 {
+		displayStuff("You are %v which is less than 18, hence too young for this", age)
+	}
 }
