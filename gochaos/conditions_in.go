ea514ccd039fa62820d7810bd062b633942f360f
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-17 23:40:56 +0200

    going gooooooooo �conditions¨

--- gochaos/conditions_in.go
+++ gochaos/conditions_in.go
@@ -13,16 +13,25 @@ func main() {
 	// logical operators : && || !
 
 	age := 15
+	number := 50
 
 	if age < 18 {
 		displayStuff("You are %v which is less than 18, hence too young for this\n", age)
 	}
 
 	if age >= 10 && age <=16 {
-		displayStuff("you must be in high school")
+		displayStuff("you must be in high schoo\n")
 	} else if age == 10 {
 		 displayStuff(" go watch ben 10")
 	}else {
 		displayStuff("i just dont know")
+	}
+
+	if number > 10 {
+		displayStuff(" number is greater than 10 ")
+	} else if number > 20 {
+		displayStuff(" %v is 20 upwards ", number)
+	}else {
+		displayStuff("NUmber is less than %v ", number)
 	}
 }
