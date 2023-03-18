a90512a663d713018dd40f5d177c7817c705bc68
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-18 10:17:25 +0200

    runes utf8 in goooooooooooooooooooooooo💨

--- gochaos/runes_in.go
+++ gochaos/runes_in.go
@@ -1,12 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
+	"unicode/utf8"
 )
 
 var printer = fmt.Printf
 
 func main() {
 	// lets work with runes and strings
-	
+
+	abcdef := "abcdef"
+	characterRune := utf8.RuneCountInString(abcdef)
+
+	print("abcdef character rune is %v:\n", characterRune)
+
 }
