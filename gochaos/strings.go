7b20bed2490e5a20f82910d369ff013c7b1771f2
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-18 09:10:13 +0200

    strings manipulations in goooooooooooooooooooooooo 🔥🔥🔥

--- gochaos/strings.go
+++ gochaos/strings.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 var print = fmt.Printf
 
 func main() {
 	// Strings in Gooooooooo
+	// some string manipulation
+
+	socialMedia := "Tweater"
+	replacer := strings.NewReplacer("a", "t")
+
+	editedSocialMedia := replacer.Replace(socialMedia)
+	print("the correct social media string is %v", editedSocialMedia)
 }
