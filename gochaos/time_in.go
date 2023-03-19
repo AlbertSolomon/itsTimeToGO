645c4890ae9db2064b1dec50452e0034271acaee
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-19 17:16:55 +0200

    go 💨 giving me c vibes 🔥

--- gochaos/time_in.go
+++ gochaos/time_in.go
@@ -11,4 +11,7 @@ func main() {
 	now := time.Now()
 
 	print("The time is %v", now)
+	print("\nThe Year is %v , Month is %v and the day is %v", now.Year(), now.Month(), now.Day())
+	print("\nThe hour is %v , Minute is %v and the second is %v", now.Hour(), now.Minute(), now.Second())
+	
 }
