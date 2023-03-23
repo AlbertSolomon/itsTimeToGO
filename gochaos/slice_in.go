e3268493320e1213c14d9475911e216a65902b90
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-23 08:56:49 +0200

    awesome go slices , ,Length and  capacity ,c😶‍🌫️

--- gochaos/slice_in.go
+++ gochaos/slice_in.go
@@ -9,9 +9,12 @@ var  print = fmt.Println
 func  main(){
 	
 	myslices := [] string {}
-	myslices = [] string {"myslices"}
-	myslices[1] = "sugar" // this spits out an error
+	myslices = [] string {"myslices", "sugarcoats"}
+	// myslices[1] = "sugar" // this spits out an error
 
 	print("myslices", myslices)
 	print("myslices from index: ", myslices[0])
+	
+	print("myslices's capacity: ", cap(myslices))
+	print("myslices's length: ", len(myslices))
 }
