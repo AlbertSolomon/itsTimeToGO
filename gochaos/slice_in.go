815e2e53accecdddc2acadaf61fdbb221e83eab1
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-23 13:59:06 +0200

    creating go slices from arrays, cap and len 🪆🪆🪆

--- gochaos/slice_in.go
+++ gochaos/slice_in.go
@@ -12,9 +12,16 @@ func  main(){
 	myslices = [] string {"myslices", "sugarcoats"}
 	// myslices[1] = "sugar" // this spits out an error
 
+	array := [...] int {10,20, 303, 33, 30, 40, 70}
+	newSlice := array[2:5]
+
 	print("myslices", myslices)
 	print("myslices from index: ", myslices[0])
 	
 	print("myslices's capacity: ", cap(myslices))
 	print("myslices's length: ", len(myslices))
+
+	print("the length of a new slice: ", len(newSlice))
+	print("the capacity of a new slice is : ", cap(newSlice))
+	print("a new slice: ", newSlice)
 }
