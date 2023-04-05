7723f3dfbb84e9d3b16dd6be61384f91697e2ad8
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-06 00:41:52 +0200

    good old loops 💨

--- gochaos/loops.go
+++ gochaos/loops.go
@@ -7,8 +7,24 @@ import (
 var printf = fmt.Println
 
 func main(){
-	
-	for i := 0; i < 10; i++ {
+
+	for i := 0; i < 4; i++ {
 		printf("solo", i)
 	}
+
+	printf("executing a function")
+	furtherLooping()
+}
+
+func furtherLooping(){
+
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			print("skipping ", i)
+			print("\n")
+			continue
+		}
+		printf("i is ", i)
+	}
+
 }
