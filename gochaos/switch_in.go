46d2134c19267437dc7fe491b0d39511322629a8
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-10 18:05:04 +0200

    go 💨 kill switch 🔥

--- gochaos/switch_in.go
+++ gochaos/switch_in.go
@@ -9,4 +9,19 @@ var printf = fmt.Println
 func main() {
 	printf("select date to excute this code ")
 
+	day := 5
+
+	switch day{
+		case 1:
+			printf("the day is monday")
+		case 2:
+			printf("the day is tuesday")
+		case 3:
+			printf("the day is wday")
+		case 4:
+			printf("the day is thursday")
+		case 5:
+			printf("the day is friiiii")
+	}
+
 }
