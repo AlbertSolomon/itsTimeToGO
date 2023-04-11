40eca6ef36d9750eb23baf2a44fb1c8eb5dbf90a
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-11 21:39:54 +0200

    making maps(make()) in go 🗾📌

--- gochaos/maps_in.go
+++ gochaos/maps_in.go
@@ -10,4 +10,10 @@ func main(){
 	// maps 
 	n := map[string] int{"potato": 30, "tomatoes":60}
 	printf(n)
+
+	makingAmap := make(map[string] int)
+	makingAmap["potato"] = 50
+	makingAmap["orange"] = 1
+
+	printf("making a map ", makingAmap)
 }
