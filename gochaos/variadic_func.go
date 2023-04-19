33f397c18d68a33a54b44d2350d9eef7c1960178
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-19 12:01:46 +0200

    variadic func in GO 💨

--- gochaos/variadic_func.go
+++ gochaos/variadic_func.go
@@ -7,7 +7,10 @@ import (
 var printf = fmt.Println
 
 func main() {
-	printf("There sum is ", sums(1,1005,60,79,90))
+	printf("There sum is ", sums(1, 1005, 60, 79, 90))
+
+	nums := [] int {50, 55, 200, 45, 7}
+	printf("sum of numbers in nums array is -> ", sums(nums...))
 }
 
 func sums(numbers... int) int {
