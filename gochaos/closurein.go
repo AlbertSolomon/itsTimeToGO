818c251c7494d681a9d79c93ee3beb10f618342e
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-19 12:23:15 +0200

    working on closures in GO 💨

--- gochaos/closurein.go
+++ gochaos/closurein.go
@@ -11,7 +11,19 @@ func main() {
 }
 
 // from go by examples
-
 func intSequence() func() int {
-	
+	seq := 0
+
+	return func() int {
+		seq ++
+		return seq
+	}
+}
+
+
+func callingintSequenceMutipletimes() {
+	numberOftimes := 10
+	for seq := 0; seq < numberOftimes; seq++ {
+		printf(intSequence())
+	}
 }
