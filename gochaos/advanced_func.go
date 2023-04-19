ce3d415d8793941a1d1e537142d7cdfc767e85dd
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-19 11:36:06 +0200

    returning multiple values in func in GO 💨

--- gochaos/advanced_func.go
+++ gochaos/advanced_func.go
@@ -6,8 +6,25 @@ import (
 
 var printf = fmt.Println
 
-func main() {
 
+func main() {
+	//printf("the percentages of the function are: ", percento())
+	low, high := values()
+	printf("the lower percent is:", lpercento(low))
+	printf("the higher percent is:", hpercento(high))
 }
 
 // from GO by example 
+func values()(int , int) {
+	return 30, 70
+}
+
+func lpercento(low int) int {
+	low = (low * 100) / 100
+	return low
+}
+
+func hpercento(high int) int {
+	high = (high * 100) / 100
+	return high
+}
