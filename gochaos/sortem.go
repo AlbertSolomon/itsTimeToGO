bdd8f7fd3ea4d16732d413ba06ff8a5c171dd8a7
AlbertSolomon <solomonaalbert@gmail.com>
2023-06-24 20:59:18 +0200

    simple bubble sort in goooooo

---
+++ gochaos/sortem.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"fmt"
+	
+)
+
+var print = fmt.Println
+
+func main() {
+	print("lets goooooo!")
+
+	// simple bubble sort in go 
+
+	collection := [...] int16 {1, 3, 10, 89, -7}
+	collectionLength := len(collection)
+
+	print(collectionLength)
+
+	for itemx := 0; itemx < collectionLength - 1; itemx++ {
+		for itemz := 0; itemz <collectionLength - 1; itemz++ {
+			if collection[itemz] > collection[itemz + 1] {
+				collection[itemz], collection[itemz+1] = collection[itemz + 1], collection[itemz]
+			}
+		}
+	}
+
+	print(collection)
+
+}
